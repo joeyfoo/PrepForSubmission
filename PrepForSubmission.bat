ce7e828b0855be9bf55a201b17a73310ff88c857
Joey Foo <joeyfoo@outlook.com>
2013-10-23 15:43:16 +0800

    Initial commit

---
+++ PrepForSubmission.bat
@@ -0,0 +1,41 @@
+echo off 
+echo PrepForSubmission.bat v1.0
+echo By Joey Foo (joeyfoo.github.io)
+echo -----
+echo This batch file is for use on the root directory of a Visual Studio solution, 
+echo and will copy the current folder out and delete unnecessasary files (\Debug, 
+echo opensdb, ncb, suo .etc) in preparation for zipping and submission online. 
+echo.
+echo DISCLAIMER: This batch file is provided AS-IS, it's your responsibility 
+echo to double-check before submission!
+echo.
+echo ----------
+
+echo.
+echo Enter the output directory name (usually your admin number for submission).
+set /p outputDir=
+echo.
+echo Starting...
+
+rmdir ..\%outputDir% /s /q
+
+mkdir ..\%outputDir%
+xcopy * ..\%outputDir% /s /c /i
+
+del ..\%outputDir%\*.opensdb /q
+del ..\%outputDir%\*.ncb /q
+del ..\%outputDir%\*.suo /q
+del ..\%outputDir%\*.sdf /q
+del ..\%outputDir%\*.user /q
+
+del ..\%outputDir%\.gitignore /q
+rmdir ..\%outputDir%\.git /s /q
+
+del ..\%outputDir%\*.bat /q
+
+cd ..\%outputDir%
+for /d /r . %%d in (Debug,Release) do @if exist "%%d" rd /s /q "%%d"
+
+echo.
+echo Completed. 
+pause
--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 PrepForSubmission
 =================
 
-Batch file thingy for preparing Visual Studio assignments for online submission
+A script I wrote to copy a Visual Studio solution directory, and delete unneeded files in preparation for submission on Blackboard. 
+
+# How to use
+
+Drop the .bat file in the solution folder, alongside the *.sln file, then run the batch file. 
+
